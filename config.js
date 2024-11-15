93ef5f80303da47e546040911e8c2b17099c671e
NIKO-PAMIYA <pamiyasl6@gmail.com>
2024-11-15 09:40:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "Pika~NIKO",
+    ownernumber: process.env.Owner_Number || "94704020146",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
@@ -29,8 +29,8 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://pamidukeshara27:1234@cluster0.h7fko.mongodb.net/",
+    sessionId: process.env.SESSION_ID || "ENBlEIr_A_N_Y_A_V_2_qdWuEd6AIXYr8",
 
     /**
      * thumbnails and medias
